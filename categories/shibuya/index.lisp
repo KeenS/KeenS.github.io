92e0d2104735bcf2a132a0bc4343bc92a25ba49e
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2017-03-27 12:11:00 +0900

    Site updated at 2017-03-27 12:11:00 UTC

--- categories/shibuya/index.lisp
+++ categories/shibuya/index.lisp
@@ -7,11 +7,16 @@
 
         <title>Shibuya.Lisp | κeenのHappy Hacκing Blog</title>
         <link rel="stylesheet" href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' type='text/css' />
-        <link rel="stylesheet" href="http://keens.github.io//libraries/normalize.3.0.1.css" />
-        <link rel="stylesheet" href="http://keens.github.io//css/liquorice.css" />
-        <link rel="shortcut icon" href="http://KeenS.github.io/favicon.ico" />
-        <link rel="apple-touch-icon-precomposed" href="http://KeenS.github.io/apple-touch-icon-144-precomposed.png" sizes="144x144" />
-        <link rel="alternate" href="http://KeenS.github.io/categories/shibuya.lisp/index.xml" type="application/rss+xml" title="κeenのHappy Hacκing Blog" />
+        <link rel="stylesheet" href="/libraries/normalize.3.0.1.css" />
+        <link rel="stylesheet" href="/css/liquorice.css" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="/manifest.json">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="theme-color" content="#ffffff">
+        <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-144-precomposed.png" sizes="144x144" />
+        <link rel="alternate" href="/categories/shibuya.lisp/index.xml" type="application/rss+xml" title="κeenのHappy Hacκing Blog" />
         
         <script>
          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -24,8 +29,8 @@
 
         </script>
         
-        <link rel="stylesheet" href="http://keens.github.io//highlight.js/styles/xcode.css" />
-<script src="http://keens.github.io//highlight.js/highlight.min.js"></script>
+        <link rel="stylesheet" href="/highlight.js/styles/xcode.css" />
+<script src="/highlight.js/highlight.min.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>
 <script type="text/x-mathjax-config">
  MathJax.Hub.Config({ tex2jax: { inlineMath: [['$','$'], ["\\(","\\)"]] } });
@@ -43,16 +48,18 @@
         <div class="row">
             <div class="sixteen columns">
                 <div class="li-brand li-left">
-                <a href="http://keens.github.io//">κeenのHappy Hacκing Blog</a> | Lispエイリアンの狂想曲</div>
+                <a href="/">κeenのHappy Hacκing Blog</a> | Lispエイリアンの狂想曲</div>
                 <div class="li-menu li-right">
                     <span class="li-menu-icon" onclick="javascript:toggle('menu');">&#9776;</span>
                     <ul id="menu2" class="li-menu-items">
                         
-                            <li><a href="http://KeenS.github.io/about"> About </a></li>
+                            <li><a href="/about/"> About </a></li>
                         
-                            <li><a href="http://KeenS.github.io/post/"> Blog </a></li>
+                            <li><a href="/index.xml"> Atom </a></li>
                         
-                            <li><a href="http://KeenS.github.io/slide/"> Slide </a></li>
+                            <li><a href="/post/"> Blog </a></li>
+                        
+                            <li><a href="/slide/"> Slide </a></li>
                         
                     </ul>
                 </div>
@@ -62,11 +69,13 @@
             <div class="sixteen columns">
                 <ul id="menu" class="li-menu-items li-menu-mobile">
                     
-                        <li><a href="http://KeenS.github.io/about"> About </a></li>
+                        <li><a href="/about/"> About </a></li>
                     
-                        <li><a href="http://KeenS.github.io/post/"> Blog </a></li>
+                        <li><a href="/index.xml"> Atom </a></li>
                     
-                        <li><a href="http://KeenS.github.io/slide/"> Slide </a></li>
+                        <li><a href="/post/"> Blog </a></li>
+                    
+                        <li><a href="/slide/"> Slide </a></li>
                     
                 </ul>
             </div>
@@ -78,40 +87,43 @@
     <div class="container">
         <div class="row">
             <div class="sixteen columns">
-                <h1>Shibuya.Lisp</h1>
+                <style>
+                 .li-rss:hover:after{content:'を購読する';font-size: 30%}
+                </style>
+                <h1 class="li-rss"><a href="/categories/shibuya/index.lisp/index.xml">Shibuya.Lisp</a></h1>
                 <ul class="li-article-list">
                     
                     <li>
-                            <h4><a href="http://keens.github.io/blog/2014/09/16/lisp-tech-talk-8">Shibuya.lisp Tech Talk #8を運営&amp;LTしてきました</a></h4>
-                            <div class="li-article-meta">
+                        <h4><a href="/blog/2014/09/16/lisp-tech-talk-8">Shibuya.lisp Tech Talk #8を運営&amp;LTしてきました</a></h4>
+                        <div class="li-article-meta">
     <time class="li-article-date">2014-09-16</time>
     <ul class="li-article-tag">
     
         <li>
-            <a href="http://KeenS.github.io/categories/shibuya.lisp">Shibuya.lisp</a>
+            <a href="/categories/shibuya.lisp">Shibuya.lisp</a>
         </li>
     
         <li>
-            <a href="http://KeenS.github.io/categories/lisp">Lisp</a>
+            <a href="/categories/lisp">Lisp</a>
         </li>
     
         <li>
-            <a href="http://KeenS.github.io/categories/common-lisp">Common Lisp</a>
+            <a href="/categories/common-lisp">Common Lisp</a>
         </li>
     
         <li>
-            <a href="http://KeenS.github.io/categories/mocl">mocl</a>
+            <a href="/categories/mocl">mocl</a>
         </li>
     
         <li>
-            <a href="http://KeenS.github.io/categories/android">Android</a>
+            <a href="/categories/android">Android</a>
         </li>
     
 </ul>
 
 </div>
 
-                        </li>
+                    </li>
                     
                 </ul>
             </div>
@@ -123,7 +135,7 @@
         <div class="row">
             <div class="sixteen columns">
                 <div class="li-page-footer-legal">
-                    &copy; 2015. All rights reserved. 
+                    &copy; 2017. All rights reserved. 
                 </div>
                 <div class="li-page-footer-theme">
                     <span class=""><a href="https://github.com/eliasson/liquorice/">liquorice</a> is a theme for <a href="http://hugo.spf13.com">hugo</a></span>
